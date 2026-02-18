725dbaaf241f30c3f6c8d307e839932d5a4b8366
Lokesh borse <149431066+lokesh-borse@users.noreply.github.com>
2026-02-18 12:27:36 +0530

    Refactor database connection and input handling
    -

--- billgeneratedb.py
+++ billgeneratedb.py
@@ -1,35 +1,47 @@
 import pyodbc
 import datetime
 import os
+import sys
 
 server = r'LOKESH\SQLEXPRESS'
 database = 'BILL_GENERATION'
 driver = '{ODBC Driver 17 for SQL Server}'
+try:
+    con = pyodbc.connect(
+        f'DRIVER={driver};'
+        f'SERVER={server};'
+        f'DATABASE={database};'
+        f'Trusted_Connection=yes;'
+    )
 
-con = pyodbc.connect(
-    f'DRIVER={driver};'
-    f'SERVER={server};'
-    f'DATABASE={database};'
-    f'Trusted_Connection=yes;'
-)
-
-print("Database Connected Successfully")
+except:
+    print('NOT connected')
+else:
+    print("Database Connected Successfully")
 
 
 class Admin:
 
     def create_customer(self):
-        name = input("Enter Customer Name: ")
-        cursor = con.cursor()
-        cursor.execute("INSERT INTO customer (c_name) VALUES (?)", (name,))
-        con.commit()
-        cursor.execute("SELECT @@IDENTITY")
-        c_id = cursor.fetchone()[0]
-        print("Customer Created with ID:", c_id)
+        try:
+            name = input("Enter Customer Name: ")
+            cursor = con.cursor()
+            cursor.execute("INSERT INTO customer (c_name) VALUES (?)", (name,))
+            con.commit()
+            cursor.execute("SELECT @@IDENTITY")
+            c_id = cursor.fetchone()[0]
+            print("Customer Created with ID:", c_id)
+        except:
+            print('Enter valid records')
         return c_id
 
 class Order:
+    #define `t_o()`
     def take_order(self, c_id):
+        '''
+        Docstring for take_order
+        This function takes the order
+         '''
         cursor = con.cursor()
         while True:
             print("\n------ MENU ------")
@@ -54,11 +66,19 @@ class Order:
                 (quantity, menu_id, c_id)
             )
             con.commit()
-            more = input("Add more items? (Y/N): ")
-            if more.upper() != 'Y':
-                break
+            try:
+                more = input("Add more items? (Y/N): ")
+                if more.upper() != 'Y':
+                    break
+            except:
+                print('Invalid Input')
 class Bill:
+    #define `g_bill()`
     def generate_bill(self, c_id):
+        '''
+        Docstring for generate_bill
+        This function generates the bill
+        '''
         cursor = con.cursor()
         cursor.execute("""
             SELECT m.m_name, o.quant, m.m_price
@@ -76,12 +96,17 @@ class Bill:
         for row in records:
             item_total = row[1] * row[2]
             total += item_total
-            print(f"{i}. {row[0]} x {row[1]} = {item_total}")
+            print(f"{i}. {row[0]} | {row[1]} = {item_total}")
             i += 1
         print("------------------------------")
         print("Total Amount:", total)
         print("==============================")
+        #define `p_bill()`
     def print_bill(self, c_id):
+        '''
+        Docstring for print_bill
+        This function prints the bill into the file 
+        '''
         cursor = con.cursor()
         cursor.execute("""
             SELECT m.m_name, o.quant, m.m_price
@@ -113,14 +138,20 @@ if __name__ == "__main__":
     admin = Admin()
     order = Order()
     bill = Bill()
-    choice = input("Do you want to order? (Y/N): ")
-    if choice.upper() == "Y":
-        c_id = admin.create_customer()
-        order.take_order(c_id)
-        view = input("Print bill to file? (Y/N): ")
-        if view.upper() == "Y":
-            bill.print_bill(c_id)
+
+    try:
+        choice = input("Do you want to order? (Y/N): ")
+        if choice.upper() == "Y":
+            c_id = admin.create_customer()
+            order.take_order(c_id)
+            view = input("Print bill to file? (Y/N): ")
+            if view.upper() == "Y":
+                bill.print_bill(c_id)
+            else:
+                bill.generate_bill(c_id)
         else:
-            bill.generate_bill(c_id)
-    else:
-        print("Thank you for visiting")
+            print("Thank you for visiting")
+    except:
+        print(sys.exc_info())
+    finally:
+        con.close()
